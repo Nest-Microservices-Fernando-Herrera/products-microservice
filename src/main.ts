e95b246a907c867a38b2279bc7b5035390bb4b59
José Barrera <josebarrerag1996@gmail.com>
2024-05-15 11:32:08 -0300

    Primer commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Levantando el servidor
+  await app.listen(3000);
+}
+bootstrap();
