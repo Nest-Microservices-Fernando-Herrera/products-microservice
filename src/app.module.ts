19d45107bd0828afecd52f28b27603c81036b355
José Barrera <josebarrerag1996@gmail.com>
2024-05-15 11:36:50 -0300

    Inicio de microservicio - Products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  // Lista de los módulos que exportan los providers requeridos en este mismo módulo
+  imports: [ProductsModule],
+  // Lista de los controladores requeridos en este módulo
   controllers: [],
+  // Lista de los providers instanciados por Nest
   providers: [],
 })
 export class AppModule {}
