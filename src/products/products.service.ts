c984fa2f9dedac01124eedc6208d91c696df0e81
José Barrera <josebarrerag1996@gmail.com>
2024-05-20 12:18:41 -0300

    Paginar mediante Prisma

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 // Implementando el cliente de Prisma e inyectarlo
@@ -23,9 +24,28 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  findAll() {
-    // Traer todos los Products
-    return this.product.findMany({});
+  async findAll(paginationDto: PaginationDto) {
+    // Desestructurando el DTO
+    const { page, limit } = paginationDto;
+
+    // Obtener la cantidad total de Products
+    const totalProducts = await this.product.count();
+
+    // Obtener la última página
+    const lastPage = Math.ceil( totalProducts / limit );
+
+    return {
+      data: await this.product.findMany({
+        // Filtros de paginación
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+      meta: {
+        total: totalProducts,
+        page: page,
+        lastPage: lastPage,
+      }
+    }
   }
 
   findOne(id: number) {
