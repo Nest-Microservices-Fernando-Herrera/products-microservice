3599d14ef5e6aee1a12379225e6e6f809d99df41
José Barrera <josebarrerag1996@gmail.com>
2024-05-24 11:51:15 -0300

    Transformar a Microservicio

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   // Manejando los logs de esta sección
   const logger = new Logger('Main');
 
-  // Creando la instancia de la app
-  const app = await NestFactory.create(AppModule);
+  // Creando la instancia de la app e implementar el microservicio
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    // Módulo principal de entrada
+    AppModule,
+    {
+      /* Opciones de configuración */
+      transport: Transport.TCP, // Medio de transporte,
+      options: {
+        port: envs.port
+      }
+    }
+  );
 
   // Configuración global de los Pipes
   app.useGlobalPipes(new ValidationPipe({
@@ -18,7 +29,7 @@ async function bootstrap() {
   }))
 
   // Levantando el servidor
-  await app.listen(envs.port);
-  logger.log(`Products Microservice running on port ${ envs.port }`);
+  await app.listen();
+  logger.log(`Products Microservice running on port ${envs.port}`);
 }
 bootstrap();
